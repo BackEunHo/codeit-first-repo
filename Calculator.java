b0adf543b6392e86f921777c4c4516c6bbccc32d
wangcoJiin <mf0d070208@gmail.com>
2025-03-28 10:48:11 +0900

    feature/test branch: 자바 계산기 - 덧셈 내용 추가

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Calculator {
+    // 덧셈
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+
+        System.out.println("더하기 결과: " + add(a, b));
+    }
+
+}
